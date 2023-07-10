02969dccf2306c54b7085f1c8b1b6160b151297e
Peter Lyn <plyn8750@gmail.com>
2023-07-10 18:41:36 -0500

    SupaBase setup and configuration as well as types defs

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import Sidebar from "@/components/Sidebar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
+import SupaBaseProvider from "@/providers/SupaBaseProvider";
+import UserProvider from "@/providers/UserProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -18,7 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>{children}</Sidebar>
+        <SupaBaseProvider>
+          <UserProvider>
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
+        </SupaBaseProvider>
       </body>
     </html>
   );
